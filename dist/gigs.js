12725fe3ae084b3e6d72e3856b57d06595683911
Erik Teichmann <erik@inverseparadox.net>
2025-11-11 09:56:13 -0500

    Only run purge on success Bump version on gigs.json include

--- dist/gigs.js
+++ dist/gigs.js
@@ -10,7 +10,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
 	}
 
 	// Fetch the gigs data from gigs.json
-	fetch( 'gigs.json?v=20251110' )
+	fetch( 'gigs.json?v=1762872924' )
 	.then( function( response ) {
 		if ( ! response.ok ) {
 			throw new Error( 'Network response was not ok.' );
