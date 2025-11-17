e2a9337c1b8a0d9dd87eb473b14a6563e17faa0d
Erik Teichmann <erik@inverseparadox.net>
2025-11-17 09:03:28 -0500

    Adds poster for 2/28

--- dist/gigs.js
+++ dist/gigs.js
@@ -10,7 +10,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
 	}
 
 	// Fetch the gigs data from gigs.json
-	fetch( 'gigs.json?v=1763133173' )
+	fetch( 'gigs.json?v=1763388188' )
 	.then( function( response ) {
 		if ( ! response.ok ) {
 			throw new Error( 'Network response was not ok.' );
